ff246a4b5b335b03d9743efcc6cf26aa5eb5e01c
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-14 16:44:09 +0530

    Add Brand/About section to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,22 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        {/* Footer sections will be added later */}
+        {/* Brand/About */}
+        <div className={styles.footerAbout}>
+        <div className={styles.brand}>
+            <h2>Pawfect Home</h2>
+            <img
+            src="src/assets/PawfectHome-Logo.png"
+            alt="Pawfect Home Logo"
+            className={styles.brandLogo}
+            />
+        </div>
+        <p>
+            We rescue and rehome abandoned pets to loving families.<br />
+            Every pet deserves a chance at happiness and a forever home filled with love.
+        </p>
+        </div>
+        
       </div>
       <div className={styles.footerBottom}>
         © 2025 Pawfect Home. All Rights Reserved.<br />
