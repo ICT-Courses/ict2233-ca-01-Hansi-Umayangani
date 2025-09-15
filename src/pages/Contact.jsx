4751a6768762b7fe21a06605544c4c5c4da91bc4
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-15 15:54:13 +0530

    Create Contact page skeleton with Navbar and Footer

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        {/* Page content will go here */}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
