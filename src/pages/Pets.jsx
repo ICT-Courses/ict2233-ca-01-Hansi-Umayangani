29c9bdf568e3d62a113373f1e6a6af5f86bb4b1f
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-15 11:04:36 +0530

    Import Link from react-router-dom to resolve ReferenceError - Added `import { Link } from 'react-router-dom'` in Pets.jsx - Enables proper functioning of navigation buttons (Contact Us, About Us)

--- src/pages/Pets.jsx
+++ src/pages/Pets.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { usePets } from "../hooks/usePets";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
